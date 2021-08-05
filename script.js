554deef0b56edbcaf9481370216f7ff02bc926d0
sean-graff <sean.graff21@gmail.com>
2021-08-05 16:20:27 -0400

    final push

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let baseURL = "http://api.coinlayer.com/"
-let key = "bc87d433c2bd265b7a200fbf33945804"
+const baseURL = "https://api.giphy.com/v1/gifs/random?api_key=oSFbwxD6VzKSAYwzQHlqsqo06lm0LkYv"
 
+
+let eye = document.getElementById('searchPic'); 
+let gifResults = document.querySelector('.gifResults');
+eye.addEventListener('click', fetchGif);
+
+async function fetchGif() {
+    let response = await fetch(baseURL);
+    let img = await response.json();  
+
+    
+    let displayGif = document.getElementById('gifResults');
+    
+    displayGif.src= img.data.images.downsized.url
+    
+    console.log(img.data.images.downsized.url)
+
+    console.log(img);
+
+}
